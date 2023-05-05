c84b17adb944617709e5e038ea71b80d24154407
Casper <nobodymystery25@gmail.com>
2023-05-05 21:46:57 +0300

    Commit

--- Açık Lise/script.js
+++ Açık Lise/script.js
@@ -1,51 +1,88 @@
-const kutular = document.querySelector(".sliding-box-container");
-const kutuKontrolleri = document.querySelector(".box-control");
-const kutuKontrolSol = document.querySelector(".box-control .prev-box");
-const kutuKontrolSag = document.querySelector(".box-control .next-box");
+const slideContainer = document.querySelector('.sliding-box-container');
+const slides = Array.from(document.querySelectorAll('.sliding-box'));
+const boxControl = document.querySelector('.box-control');
+const nextBtn = document.querySelector('.next-box');
+const prevBtn = document.querySelector('.prev-box');
 
-let kutuIndex = 0;
-let kutuGenislik = kutular.querySelector(".sliding-box").offsetWidth;
-let dokunmaX;
-let kaydirmaX;
+let currentSlide = 0;
+let isDragging = false;
+let startPos = 0;
+let currentTranslate = 0;
+let prevTranslate = 0;
+let animationID = 0;
+let slideWidth = slideContainer.offsetWidth / 3;
+const slidesToShow = 3;
 
-kutuKontrolSol.addEventListener("click", () => {
-  kutuIndex--;
-  kaydir();
+slides.forEach((slide, index) => {
+  const slideImage = slide.querySelector('img');
+  slideImage.addEventListener('dragstart', (e) => e.preventDefault());
+
+  slide.addEventListener('mousedown', mousedown);
+  slide.addEventListener('mouseup', mouseup);
+  slide.addEventListener('mouseleave', mouseup);
+  slide.addEventListener('mousemove', mousemove);
 });
 
-kutuKontrolSag.addEventListener("click", () => {
-  kutuIndex++;
-  kaydir();
-});
+function mousedown(event) {
+  startPos = event.clientX;
+  isDragging = true;
 
-kutular.addEventListener("touchstart", (e) => {
-  const ilkDokunma = e.touches[0];
-  dokunmaX = ilkDokunma.clientX;
-});
-
-kutular.addEventListener("touchmove", (e) => {
-  const sonDokunma = e.touches[0];
-  kaydirmaX = dokunmaX - sonDokunma.clientX;
-});
-
-kutular.addEventListener("touchend", () => {
-  if (kaydirmaX > 5) {
-    kutuIndex++;
-    kaydir();
-  } else if (kaydirmaX < -5) {
-    kutuIndex--;
-    kaydir();
-  }
-});
-
-function kaydir() {
-  if (kutuIndex < 0) {
-    kutuIndex = 0;
-  } else if (kutuIndex > kutular.children.length - 1) {
-    kutuIndex = kutular.children.length - 1;
-  }
-
-  kutular.style.transform = `translateX(-${kutuIndex * (kutuGenislik + 10)}px)`;
+  animationID = requestAnimationFrame(animation);
+  slideContainer.classList.add('grabbing');
 }
 
-kaydir();
+function mouseup() {
+  isDragging = false;
+  cancelAnimationFrame(animationID);
+
+  const movedBy = currentTranslate - prevTranslate;
+
+  if (movedBy < -100 && currentSlide < slides.length - slidesToShow) {
+    currentSlide += 1;
+  }
+
+  if (movedBy > 100 && currentSlide > 0) {
+    currentSlide -= 1;
+  }
+
+  setPositionByIndex();
+
+  slideContainer.classList.remove('grabbing');
+}
+
+function mousemove(event) {
+  if (isDragging) {
+    const currentPosition = event.clientX;
+    currentTranslate = prevTranslate + currentPosition - startPos;
+  }
+}
+
+function animation() {
+  setSliderPosition();
+  if (isDragging) requestAnimationFrame(animation);
+}
+
+function setSliderPosition() {
+  slideContainer.style.transform = `translateX(${currentTranslate}px)`;
+}
+
+function setPositionByIndex() {
+  currentTranslate = currentSlide * -slideWidth;
+  prevTranslate = currentTranslate;
+  setSliderPosition();
+}
+
+function slideForward() {
+  if (currentSlide >= slides.length - slidesToShow) return;
+  currentSlide++;
+  setPositionByIndex();
+}
+
+function slideBackward() {
+  if (currentSlide <= 0) return;
+  currentSlide--;
+  setPositionByIndex();
+}
+
+nextBtn.addEventListener('click', slideForward);
+prevBtn.addEventListener('click', slideBackward);
