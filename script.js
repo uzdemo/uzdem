fae296e83bf9402a3dc6684dfe625d04e94c2fad
Casper NIRVANA <falanzat@outlook.com>
2023-06-11 00:04:26 +0300

    Keşfet sayfası tasarımı

--- script.js
+++ script.js
@@ -42,3 +42,46 @@ function nextSlide() {
     currentSlide++;
   }
 }
+
+/* Satır Kaydırma Kodları */
+
+// Kayan satırları seçme
+const kayanSatirlar = document.querySelectorAll(".satir");
+
+kayanSatirlar.forEach((satir) => {
+  // Satırdaki kutu alanını, kontrol düğmelerini ve sola ve sağa kaydırma düğmelerini seçme
+  const kutular = satir.querySelector(".icerik");
+  const kutuKontrolSol = satir.querySelector(".onceki");
+  const kutuKontrolSag = satir.querySelector(".sonraki");
+
+  let kutuIndex = 0;
+  let kutuGenislik = kutular.querySelector(".kutu").offsetWidth;
+
+  // Sol kaydırma düğmesine tıklama olayı
+  kutuKontrolSol.addEventListener("click", () => {
+    kutuIndex--;
+    kaydir();
+  });
+
+  // Sağ kaydırma düğmesine tıklama olayı
+  kutuKontrolSag.addEventListener("click", () => {
+    kutuIndex++;
+    kaydir();
+  });
+
+  // Kutuları yatay olarak kaydırma fonksiyonu
+  function kaydir() {
+    if (kutuIndex < 0) {
+      // İndeks sınırlarını kontrol etme ve negatif değerleri sıfıra eşitleme
+      kutuIndex = 0;
+    } else if (kutuIndex > kutular.children.length - 1) {
+      // İndeks sınırlarını kontrol etme ve fazla değerleri son kutu indeksiyle sınırlama
+      kutuIndex = kutular.children.length - 1;
+    }
+
+    // Kutuları yatay olarak kaydırma
+    kutular.style.transform = `translateX(-${kutuIndex * (kutuGenislik + 30)}px)`;
+  }
+
+  kaydir();
+});
